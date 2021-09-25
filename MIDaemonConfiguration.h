3386bf4677b0bdc4287e77be59e8389ad2d3e09a
daniel <dlevi309@gmail.com>
2021-09-24 21:58:13 -0700

    first blood for real

--- MIDaemonConfiguration.h
+++ MIDaemonConfiguration.h
@@ -1,15 +1,11 @@
-@interface MIGlobalConfiguration : NSObject {}
+@interface MIGlobalConfiguration : NSObject
+
 @end
 
-@interface MIDaemonConfiguration : MIGlobalConfiguration {}
-@property (nonatomic,readonly) BOOL skipDeviceFamilyCheck; 
-@property (nonatomic,readonly) BOOL skipThinningCheck; 
-@property (nonatomic,readonly) BOOL allowPatchWithoutSinf; 
-@property (nonatomic,readonly) BOOL codeSigningEnforcementIsDisabled; 
-@property (nonatomic,readonly) BOOL isInternalImageType;
--(BOOL)isInternalImageType;
--(BOOL)codeSigningEnforcementIsDisabled;
--(BOOL)skipDeviceFamilyCheck;
--(BOOL)skipThinningCheck;
--(BOOL)allowPatchWithoutSinf;
+@interface MIDaemonConfiguration : MIGlobalConfiguration
+
+@property (nonatomic, readonly) BOOL skipDeviceFamilyCheck; 
+@property (nonatomic, readonly) BOOL skipThinningCheck; 
+@property (nonatomic, readonly) BOOL allowPatchWithoutSinf; 
+
 @end
